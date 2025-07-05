71d937bf26de47652b1898ead30325e2698dc542
trindadedev13 <trindadedev13@gmail.com>
2025-07-05 19:36:43 -0300

    feat(EditorDefaults): Add int, float, long and remove number

--- editor/src/main/java/mo/kilate/editor/KilateEditorDefaults.java
+++ editor/src/main/java/mo/kilate/editor/KilateEditorDefaults.java
@@ -15,7 +15,9 @@ public class KilateEditorDefaults {
           new KilateAutoCompleteItem("false", KilateAutoCompleteType.Keyword),
           new KilateAutoCompleteItem("true", KilateAutoCompleteType.Keyword),
           new KilateAutoCompleteItem("string", KilateAutoCompleteType.Type),
-          new KilateAutoCompleteItem("number", KilateAutoCompleteType.Type),
+          new KilateAutoCompleteItem("int", KilateAutoCompleteType.Type),
+          new KilateAutoCompleteItem("float", KilateAutoCompleteType.Type),
+          new KilateAutoCompleteItem("long", KilateAutoCompleteType.Type),
           new KilateAutoCompleteItem("bool", KilateAutoCompleteType.Type),
           new KilateAutoCompleteItem("any", KilateAutoCompleteType.Type),
           new KilateAutoCompleteItem("print", KilateAutoCompleteType.Function));
